78358d6078d2909ba8ec9a69deca1f875de5b25f
Sorpirit <danvuk11@gmail.com>
2019-07-25 20:38:30 +0300

    + Joystick, PlayerMovementController

---
+++ TopDownShooer/Assets/Scripts/PlayerMovemenController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovemenController : MonoBehaviour
+{
+    [SerializeField]
+    private FixedJoystick joystick;
+    [SerializeField]
+    private float speed;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector3(joystick.Horizontal, joystick.Vertical) * speed * Time.deltaTime);
+    }
+
+}
