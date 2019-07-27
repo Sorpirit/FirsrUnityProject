213db86b267b25d460ff4178a0e1edd0f5422f44
Sorpirit <danvuk11@gmail.com>
2019-07-27 12:52:20 +0300

    +Shooting, Update Movement system, Update Player Sptrite

--- TopDownShooer/Assets/Scripts/PlayerMovemenController.cs
+++ TopDownShooer/Assets/Scripts/PlayerMovemenController.cs
@@ -5,14 +5,59 @@ using UnityEngine;
 
 public class PlayerMovemenController : MonoBehaviour
 {
-    [SerializeField]
-    private FixedJoystick joystick;
-    [SerializeField]
-    private float speed;
+    [SerializeField] private FixedJoystick movementJoystick;
+    [SerializeField] private FixedJoystick faceingJoyStick;
+    [SerializeField] private float acc = 1;
+    [SerializeField] private int maxSpeed = 1;
+
+    private Rigidbody2D rb; 
+    private Vector2 moveInput;
+    private Vector2 faceInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveInput = new Vector2(movementJoystick.Horizontal, movementJoystick.Vertical);
+        faceInput = new Vector2(faceingJoyStick.Horizontal, faceingJoyStick.Vertical);
+        
+    }
 
     private void FixedUpdate()
     {
-        transform.Translate(new Vector3(joystick.Horizontal, joystick.Vertical) * speed * Time.deltaTime);
+        
+        if(faceInput != Vector2.zero)
+        {
+            transform.up = faceInput.normalized;
+        }
+        else if(moveInput != Vector2.zero)
+        {
+            transform.up = moveInput.normalized;
+        }
+
+        if (moveInput != Vector2.zero)
+        {
+
+            Move();
+
+        }
+        else if (rb.velocity != Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+
+    }
+
+    private void Move()
+    {
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            rb.velocity += (Vector2)moveInput.normalized * acc * Time.deltaTime;
+        }
     }
 
 }
---
+++ TopDownShooer/Assets/Scripts/PlayerShootingController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingController : MonoBehaviour
+{
+
+    [SerializeField] private Transform shootingPoint;
+    [SerializeField] private float damage = 5;
+
+    [SerializeField] private float sensivity = 0.46f;
+    [SerializeField] private FixedJoystick joystick;
+    [SerializeField] private LineRenderer fireLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(joystick.Horizontal, joystick.Vertical);
+        if (input != Vector2.zero) {
+
+            if (input.magnitude >= sensivity)
+                {
+                    StartCoroutine(Shoot());
+                }
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(shootingPoint.position,shootingPoint.up);
+
+        if (hitInfo)
+        {
+            
+            fireLine.SetPosition(0, shootingPoint.parent.position);
+            fireLine.SetPosition(1, shootingPoint.parent.position + shootingPoint.up * 100);
+
+            fireLine.enabled = true;
+
+            yield return new WaitForSeconds(0.02f);
+            
+            fireLine.enabled = false;
+        }
+    }
+}
